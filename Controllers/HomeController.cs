1ea79b3643a0ca8d4f1c567fddea8d5fc6d22273
Samantha Prettyman <spretty1@users.noreply.github.com>
2021-06-15 10:47:51 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http; 
+using ActioItemWebAPI.Models;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ActioItemWebAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private NumberDBEntities context = new NumberDBEntities();
+        //public IEnumerable <SequenceModel> Get ()
+        //{
+        //    IEnumerable<SequenceModel> list = null; 
+
+        //    list = (from c in context.Series select new SequenceModel)
+        //}
+
+        public ActionResult GetJsonDataModel()
+        {
+            foreach (var itm in Model)
+            {
+                var webClient = new WebClient();
+                webClient.Headers.Add(HttpRequestHeader.Cookie, "cookievalue");
+                var json = webClient.DownloadString(@"https://jsonplaceholder.typicode.com/todos/1");
+                Models.NumberDBEntities objJson = JsonConvert.DeserializeObject<Models.NumberDBEntities>(json);
+                return View("Index", objJson);
+            }
+        }
+    }
+}
---
+++ Models/NumberDBEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActioItemWebAPI.Models
+{
+    public class NumberDBEntities
+    {
+        public int Number { get; set; }
+        public string Series { get; set; }
+    }
+}
